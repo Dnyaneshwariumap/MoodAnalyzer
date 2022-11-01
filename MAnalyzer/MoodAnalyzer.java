12db74efe28594dc2e77f6b0a4c71cc89d63f504
Dnyaneshwariumap <dnyaneshwariumap994@gmail.com>
2022-11-01 12:16:53 +0530

    welcome

---
+++ MAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,12 @@
+package MAnalyzer;
+
+public class MoodAnalyzer {
+        public static void main(String[]args){
+            System.out.println("welcome to mood analyzer system");
+
+        }
+    }
+
+
+
+
