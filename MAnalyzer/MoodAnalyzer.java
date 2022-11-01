cae82b4aad38097d11e4149b7867c160851dfc5b
Dnyaneshwariumap <dnyaneshwariumap994@gmail.com>
2022-11-01 15:31:56 +0530

    Added: Test cases to analyze happy or sad mood

--- MAnalyzer/MoodAnalyzer.java
+++ MAnalyzer/MoodAnalyzer.java
@@ -1,12 +1,33 @@
 package MAnalyzer;
 
 public class MoodAnalyzer {
-        public static void main(String[]args){
-            System.out.println("welcome to mood analyzer system");
+    String mood;
 
-        }
+    public MoodAnalyzer() {
     }
 
+    public MoodAnalyzer(String mood) {
+        this.mood = mood;
+    }
+
+    public String analyzeMood(){
+        try {
+            if (mood.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+        catch(Exception e){
+            return "Happy";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("-----------------------Welcome to Mood Analysis Problem!-----------------------i");
+    }
+
+}
 
 
 
